5ab6b1ea5cc64f26d515c6e9fc7b82a0be6bb683
DESKTOP-NHF6DQ6\Schab <Julia Schabasser>
2022-08-21 12:50:21 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddSensorsDataComponent } from './dashboard/add-sensors-data/add-sensors-data.component';
+import { ButtonComponent } from './button/button.component';
+import { HeaderComponent } from './header/header.component';
+import { SensorsDataComponent } from './dashboard/sensors-data/sensors-data.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AddSensorsDataComponent,
+    ButtonComponent,
+    HeaderComponent,
+    SensorsDataComponent,
+    AboutPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
